fa6565d1b1606b5103ad9c7336305075ab3bae0d
vanbungkring <prasetyo.mailbox@gmail.com>
2020-07-31 06:49:19 +0700

    add linter rule on the project

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 import sharp from 'sharp';
+import { logger } from './core/util/logger';
 
-
-const img = sharp('./sample.jpeg')
+const img = sharp('./sample.jpeg');
 img.metadata().then((meta: any) => {
-  console.info(meta);
+  logger.info(meta);
   return img
     .resize(Math.round(meta.width / 2))
     .grayscale()
     .rotate(90)
     .withMetadata()
     .png()
-    .toFile('sample-2.png', (data: any) => { 
-
-    })
+    .toFile('sample-2.png', (data: any) => {
+      logger.info(data);
+    });
 }).catch((err: Error) => {
-  console.info(err);
-})
+  logger.info(err);
+});
--- src/core/util/s3Helper.ts
+++ src/core/util/s3Helper.ts
@@ -4,34 +4,32 @@ import * as AWS from 'aws-sdk';
 interface IArdaS3Helper {
   validateBuckets(name: string): Promise<any>;
 }
-class ArdaS3Helper {
+export class ArdaS3Helper implements IArdaS3Helper {
 
-  constructor() { }
+  // constructor() { }
   async validateBuckets(name: string): Promise<any> {
     const formattedBucket: string = name.replace(/\s/g, '');
     logger.info(`trying to validate bucket with format ${formattedBucket}`);
 
     const buckets = formattedBucket.split(',');
-    const errorBuckets = []
+    const errorBuckets: any[] = [];
     return new Promise(async (resolve, reject) => {
-
-      for (let i = 0; i < buckets.length; i++) {
-
+      for (const bucket of buckets) {
         const s3 = new AWS.S3({ signatureVersion: 'v4' });
-        const params = { Bucket: buckets[i] };
-
+        const params = { Bucket: bucket };
         try {
           await s3.headBucket(params).promise();
-          logger.info(`Found bucket: ${buckets[i]}`);
+          logger.info(`Found bucket: ${bucket}`);
         } catch (err) {
-          logger.error(`Could not find bucket: ${buckets[i]}`);
-          errorBuckets.push(buckets[i]);
+          logger.error(`Could not find bucket: ${bucket}`);
+          errorBuckets.push(bucket);
         }
       }
-      if (errorBuckets.length > 0) { 
+
+      if (errorBuckets.length > 0) {
         return reject(errorBuckets);
       }
       resolve(`${formattedBucket} is valid bucket name`);
-    })
+    });
   }
 }
