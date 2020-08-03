db62aabf841863ff150343b8a505cf7f63ddefa5
vanbungkring <prasetyo.mailbox@gmail.com>
2020-08-03 08:07:46 +0700

    adding image result to ignore

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
 import sharp from 'sharp';
 import { logger } from './core/util/logger';
 
-const img = sharp('./sample.jpeg');
+const img = sharp('./test/imagesource/codercat.jpg');
 img.metadata().then((meta: any) => {
-  logger.info(meta);
   return img
     .resize(Math.round(meta.width / 2))
     .grayscale()
     .rotate(90)
     .withMetadata()
     .png()
-    .toFile('sample-2.png', (data: any) => {
+    .toFile(`./test/result/${Date.now()/1000}.png`, (data: any) => {
+      logger.info(data);
+    });
+}).catch((err: Error) => {
+  logger.info(err);
+});
+
+img.metadata().then((meta: any) => {
+  return img
+    .resize(Math.round(meta.width / 4))
+    .grayscale()
+    .rotate(90)
+    .withMetadata()
+    .png()
+    .toFile(`./test/result/${Date.now()/1000}.png`, (data: any) => {
       logger.info(data);
     });
 }).catch((err: Error) => {
